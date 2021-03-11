101681e720ad0b5e3fd02dc021b7fea02cf95054
NguyenCuuNguyen <nguyen2nk@gmail.com>
2021-03-11 01:24:47 -0500

    Floyd's tortoise and Hare algo illustration

--- detectLLCycle.py
+++ detectLLCycle.py
@@ -25,3 +25,19 @@ class Solution(object):
             head = head.next
             slow = slow.next
         return head
+
+#Consider the following linked list, where E is the cylce entry and X, the crossing point of fast and slow.
+#        H: distance from head to cycle entry E
+#        D: distance from E to X
+#        L: cycle length
+#                          _____
+#                         /     \
+#        head_____H______E       \
+#                        \       /
+#                         X_____/   
+        
+    
+#        If fast and slow both start at head, when fast catches slow, slow has traveled H+D and fast 2(H+D). 
+#        Assume fast has traveled n loops in the cycle, we have:
+#        2H + 2D = H + D + L  -->  H + D = nL  --> H = nL - D
+#        Thus if two pointers start from head and X, respectively, one first reaches E, the other also reaches E. 
