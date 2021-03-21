d1746fedd31fb812d510bbf1d8294c21500efabc
NguyenCuuNguyen <nguyen2nk@gmail.com>
2021-03-20 23:19:20 -0400

    Remove Duplicates from Sorted List II

---
+++ LinkedList/removeDup2.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode fast = temp;
+        ListNode slow = temp;
+        fast = slow.next; //fast points to first node
+        ListNode cur = temp;
+
+        while(fast != null){
+            while(fast.next != null && fast.val == fast.next.val){//find the last dup node
+                fast = fast.next;
+            } 
+            if(slow.next != fast){ 
+                slow.next = fast.next;
+             //   slow = slow.next; //if moved slow, couldn't check if duplicate.next is duplicate or not with fast
+                fast = slow.next;
+            }else{
+                slow = slow.next;
+                fast = fast.next;
+            }
+            
+        }
+        return temp.next;
+    }
+}
+
+// if(cur.next.val == cur.next.next.val){
+//                 val = cur.next.val;
+//                 cur.next = cur.next.next.next; 
+//             }else if (cur.val = ){
+                
+//             }else{
+//                 cur = cur.next;
+//             }
