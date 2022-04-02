ca1e2c851f0cb17c625e1e1e2057871ea852ccd5
NguyenCuuNguyen <nguyen2nk@gmail.com>
2022-04-02 15:41:14 -0400

    Intersection of Two Arrays II solution 2

--- Array/Intersection_of_Two_Arrays_II.py
+++ Array/Intersection_of_Two_Arrays_II.py
@@ -22,3 +22,12 @@ class Solution:
         return re
 
     
+    """96 ms, faster than 19.39% . 14.2 MB, less than 17.95%"""
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        re = []
+        count1 = collections.Counter(nums1)
+        count2 = collections.Counter(nums2)
+        keylist = list(set(nums1) & set(nums2))
+        for k in keylist:
+            re.extend(list([k] * min(count1.get(k), count2.get(k))))
+        return re
