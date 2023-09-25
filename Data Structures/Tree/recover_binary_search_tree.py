a737e16db59b6f807d7e4abf79c365197318d0f2
Iris Cuu <iris.cuu@nagarro.com>
2023-09-25 01:59:35 -0400

    problem 99

---
+++ Data Structures/Tree/recover_binary_search_tree.py
@@ -0,0 +1,39 @@
+#https://leetcode.com/problems/recover-binary-search-tree/description/
+#LESSON 1: If prompt only requires to keep track of 2 nodes, then only store 2 nodes instead of the whole array of inorder nodes as traversing. Pay close attetion to prompt!
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        pre, first, second = None, None, None
+        cur = root
+        while cur != None:
+            if not cur.left: 
+                if pre != None and pre.val > cur.val: #REVIEW
+                        if first is None:
+                            first = pre
+                        second = cur
+                pre = cur
+                cur = cur.right
+            else: #find inorder predecessor aka rightmost node of left subtree:
+                #Add a temp node to traverse & search for inorder predecssor aka rightmost node inleft substree:
+                node = cur.left
+                while node.right != None and node.right != cur:
+                    node = node.right
+                #After reaching inorder predecessor node, handle each scenario:
+                #if predecessor.right is null, Create link
+                if node.right == None:
+                    node.right = cur
+                    cur = cur.left
+                #if predecessor.right is current, Restore tree
+                else:
+                    node.right = None
+                    #Inorder predecessor or rightmost node of left subtree has to be smaller than current/root, check for that:
+                    if pre != None and pre.val > cur.val:
+                        if first is None:
+                            first = pre
+                        second = cur #WHY? because it's inside undesired condition of pre > cur
+                    pre = cur
+                    cur = cur.right
+        if first != None and second != None: #REVIEW
+            first.val, second.val = second.val, first.val
