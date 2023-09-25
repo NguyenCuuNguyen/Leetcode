54f59f64c4d91aad1e7de3ab8079326b77ee344d
Iris Cuu <iris.cuu@nagarro.com>
2023-09-25 14:31:40 -0400

    problem 99 comment

--- Data Structures/Tree/recover_binary_search_tree.py
+++ Data Structures/Tree/recover_binary_search_tree.py
@@ -1,5 +1,6 @@
 #https://leetcode.com/problems/recover-binary-search-tree/description/
-#LESSON 1: If prompt only requires to keep track of 2 nodes, then only store 2 nodes instead of the whole array of inorder nodes as traversing. Pay close attetion to prompt!
+
+#SOLUTION 1: MORRIS INORDER TRAVERSAL
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
@@ -8,12 +9,13 @@ class Solution:
         pre, first, second = None, None, None
         cur = root
         while cur != None:
-            if not cur.left: 
-                if pre != None and pre.val > cur.val: #REVIEW
-                        if first is None:
-                            first = pre
+            if not cur.left: #If reach end of left subtree root:
+                if pre != None and pre.val > cur.val: #REVIEW: 
+                    if first is None: #without this, wrong output [1,2,null,null,3]
+                        first = pre
                         second = cur
-                pre = cur
+                #First assignment of pre goes here
+                pre = cur 
                 cur = cur.right
             else: #find inorder predecessor aka rightmost node of left subtree:
                 #Add a temp node to traverse & search for inorder predecssor aka rightmost node inleft substree:
@@ -30,10 +32,12 @@ class Solution:
                     node.right = None
                     #Inorder predecessor or rightmost node of left subtree has to be smaller than current/root, check for that:
                     if pre != None and pre.val > cur.val:
-                        if first is None:
+                        if first is None: #WHY this cond? Without this, wrong output [2,3,null,null,1]. Without both, it fails for [1,3,null,null,2] because wrongly replace first=2 VS the correct first=3, second=1 in second visit to cur=1 to remove link.
                             first = pre
-                        second = cur #WHY? because it's inside undesired condition of pre > cur
-                    pre = cur
+                            second = cur #WHY? because it's inside undesired condition of pre > cur
+                    pre = cur #~visit
                     cur = cur.right
         if first != None and second != None: #REVIEW
             first.val, second.val = second.val, first.val
+
+#LESSON 1: If prompt only requires to keep track of 2 nodes, then only store 2 nodes instead of the whole array of inorder nodes as traversing. Pay close attetion to prompt!
